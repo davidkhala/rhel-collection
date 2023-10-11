a5e059f15167acc2f7fb1576170d212e6f58be80
David Liu <david-khala@hotmail.com>
2023-10-11 11:51:52 +0800

    Create license.sh

---
+++ license.sh
@@ -0,0 +1,10 @@
+re-attach(){
+  sudo subscription-manager remove --all
+  sudo subscription-manager unregister
+  sudo subscription-manager clean
+  sudo subscription-manager register
+  sudo subscription-manager refresh
+  sudo subscription-manager attach --auto  
+
+}
+$@
