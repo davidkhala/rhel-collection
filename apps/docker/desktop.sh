e5e49437147a58f32cfd4eaf3d104013cf827078
David Liu <david-khala@hotmail.com>
2025-06-02 21:26:56 +0800

    Create desktop.sh

---
+++ apps/docker/desktop.sh
@@ -0,0 +1,27 @@
+set -e
+# ref: https://docs.docker.com/desktop/setup/install/linux/rhel/
+install(){
+  sudo subscription-manager repos --enable codeready-builder-for-rhel-9-$(arch)-rpms
+  sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+  sudo dnf install pass
+
+  # For GNOME desktop environment 
+  if [ "$(printenv XDG_CURRENT_DESKTOP)" = "GNOME" ]; then
+    sudo dnf install gnome-shell-extension-appindicator
+    sudo gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+  else
+    sudo dnf install gnome-terminal
+  fi
+  sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+  wget https://desktop.docker.com/linux/main/amd64/docker-desktop-x86_64-rhel.rpm
+  sudo dnf install ./docker-desktop-x86_64-rhel.rpm
+  rm ./docker-desktop-x86_64-rhel.rpm
+
+  # post install
+  sudo ln -s /usr/local/bin/com.docker.cli /usr/bin/docker
+  sudo ln -s /usr/libexec/qemu-kvm /usr/local/bin/qemu-system-x86_64
+
+}
+
+
+$@
