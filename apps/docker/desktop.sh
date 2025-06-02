fe929097454f94f09a28ee824427464066ab424b
David Liu <david-khala@hotmail.com>
2025-06-02 21:33:39 +0800

    Update desktop.sh

--- apps/docker/desktop.sh
+++ apps/docker/desktop.sh
@@ -3,18 +3,18 @@ set -e
 install(){
   sudo subscription-manager repos --enable codeready-builder-for-rhel-9-$(arch)-rpms
   sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-  sudo dnf install pass
+  sudo dnf install -y pass
 
   # For GNOME desktop environment 
   if [ "$(printenv XDG_CURRENT_DESKTOP)" = "GNOME" ]; then
-    sudo dnf install gnome-shell-extension-appindicator
-    sudo gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+    sudo dnf install -y gnome-shell-extension-appindicator
+    sudo gnome-extensions enable david-khala@hotmail.com
   else
-    sudo dnf install gnome-terminal
+    sudo dnf install -y gnome-terminal
   fi
   sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
   wget https://desktop.docker.com/linux/main/amd64/docker-desktop-x86_64-rhel.rpm
-  sudo dnf install ./docker-desktop-x86_64-rhel.rpm
+  sudo dnf install -y ./docker-desktop-x86_64-rhel.rpm
   rm ./docker-desktop-x86_64-rhel.rpm
 
   # post install
